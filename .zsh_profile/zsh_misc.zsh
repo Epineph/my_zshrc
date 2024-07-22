fcf216b2f4c6b6c15808abb627967c49ac86459c
Epineph <heiniwinther@hotmail.com>
2024-07-22 03:53:51 +0200

    Update zsh_misc.zsh

--- .zsh_profile/zsh_misc.zsh
+++ .zsh_profile/zsh_misc.zsh
@@ -6,3 +6,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 source /usr/share/nvm/init-nvm.sh
+
+# Check if .oh-my-zsh is installed, install if not
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
