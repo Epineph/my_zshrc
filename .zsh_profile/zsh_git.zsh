eb74d674956baa4d1023cffa1b82e96607db8d88
Epineph <heiniwinther@hotmail.com>
2024-11-26 14:42:43 +0100

    Update zsh_git.zsh

--- .zsh_profile/zsh_git.zsh
+++ .zsh_profile/zsh_git.zsh
@@ -1,9 +1,9 @@
 # $HOME/.zsh_profile/zsh_git.zsh
 
-git_gpg_keyid="C582D413C389CFEB7ED7E712C10A9019600780FC"
+git_gpg_keyid="<redacted>"
 
-git config --global user.name "Heini Winther Johnsen"
-git config --global user.email "heiniwinther@hotmail.com"
+git config --global user.name "<redacted>"
+git config --global user.email "<redacted>"
 git config --global user.signingkey "$git_gpg_keyid"
 git config --global commit.gpgsign true
 git config --global color.ui auto
