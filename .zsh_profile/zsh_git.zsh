150887225f2b072254561a1eba8ee24b9741f0b6
Epineph <heiniwinther@hotmail.com>
2025-01-09 08:18:09 +0100

    commit

--- .zsh_profile/zsh_git.zsh
+++ .zsh_profile/zsh_git.zsh
@@ -10,5 +10,6 @@ git config --global color.ui auto
 git config --global init.defaultBranch main
 
 
+git config --global url."https://aur.archlinux.org/".insteadOf "ssh://aur.archlinux.org/"
 
 git config --global url."git@github.com:".insteadOf "https://github.com/"
