4e8f88f3091318865343b85603051f4ac47e9e3b
Epineph <heiniwinther@hotmail.com>
2025-01-09 01:11:59 +0100

    commit

--- .zsh_profile/zsh_git.zsh
+++ .zsh_profile/zsh_git.zsh
@@ -9,17 +9,6 @@ git config --global commit.gpgsign true
 git config --global color.ui auto
 git config --global init.defaultBranch main
 
-# configure zsh-diff-so-fancy
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-## color options
-git config --global color.ui true
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-git config --global color.diff.meta       "yellow"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
+
+
+git config --global url."git@github.com:".insteadOf "https://github.com/"
